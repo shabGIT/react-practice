2510c2123e40e27a82ec32349c859fd4b305cc2c
shabGIT <fshabnam1998@gmail.com>
2022-08-28 19:46:27 +0530

    button disabled and fork filter

--- src/Git.jsx
+++ src/Git.jsx
@@ -6,8 +6,10 @@ export const Git = () => {
   const [user, setUser] = useState("");
   const [response, setResponse] = useState({});
   const [filt, setFilt] = useState([]);
+  const [includeFork, setIncludeFork] = useState(false);
 
   const searchRepos = async () => {
+    console.log(includeFork);
     const result = await axios.get(
       `https://api.github.com/users/${user}/repos`
     );
@@ -20,13 +22,28 @@ export const Git = () => {
     console.log(response);
   }, [response]);
 
+  const shouldDisplay = (isFork) => {
+    if (includeFork) {
+      return true;
+    } else {
+      return !isFork;
+    }
+  };
+
   return (
     <div>
       <label htmlFor="">Githun Username:</label>
       <input type="text" onChange={(e) => setUser(e.target.value)} />
       <label htmlFor="">Includes forks:</label>
-      <input type="checkbox" />
-      <button onClick={searchRepos}>Search</button>
+      <input
+        type="checkbox"
+        onChange={(e) => {
+          setIncludeFork(e.target.checked);
+        }}
+      />
+      <button onClick={searchRepos} disabled={user.length === 0}>
+        Search
+      </button>
 
       <table>
         <tr>
@@ -38,13 +55,15 @@ export const Git = () => {
         </tr>
         {filt.map((item) => {
           return (
-            <tr>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>{item.description}</td>
-              <td>{item.fork + ""}</td>
-              <td>{item.size}</td>
-            </tr>
+            shouldDisplay(item.fork) && (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.description}</td>
+                <td>{item.fork + ""}</td>
+                <td>{item.size}</td>
+              </tr>
+            )
           );
         })}
       </table>
